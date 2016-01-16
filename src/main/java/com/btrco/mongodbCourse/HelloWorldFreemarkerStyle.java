497560f49123eb7e601de856ec443786eb0d5d0d
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2016-01-16 15:10:37 +0200

    Add freemarker framework and test it

---
+++ src/main/java/com/btrco/mongodbCourse/HelloWorldFreemarkerStyle.java
@@ -0,0 +1,30 @@
+package com.btrco.mongodbCourse;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldFreemarkerStyle {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
+
+        try {
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter stringWriter = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+            helloMap.put("name", "BOGDANIO");
+            helloTemplate.process(helloMap, stringWriter);
+            System.out.println(stringWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
