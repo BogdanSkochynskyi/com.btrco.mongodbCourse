776ed057cde245b19c7b25c41d75d195d5811f95
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2016-01-18 20:44:58 +0200

    Add some most common operations with MongoDB

---
+++ src/main/java/com/btrco/mongodbCourse/App.java
@@ -0,0 +1,19 @@
+package com.btrco.mongodbCourse;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.Document;
+
+public class App {
+    public static void main(String[] args) {
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(200).build();
+        MongoClient client = new MongoClient(new ServerAddress(), options);
+
+        MongoDatabase database = client.getDatabase("test").withReadPreference(ReadPreference.secondary());
+
+        MongoCollection<Document> collection = database.getCollection("test")/*.withReadPreference(ReadPreference.secondary())*/;
+        MongoCollection<BsonDocument> collectionBson = database.getCollection("test", BsonDocument.class)/*.withReadPreference(ReadPreference.secondary())*/;
+    }
+}
