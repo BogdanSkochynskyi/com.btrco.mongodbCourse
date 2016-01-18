8fa19954fa158dec5af609fcd23c0b67dc915266
bogdanskochynskyi <bogdanskochynskyi@gmail.com>
2016-01-18 21:21:29 +0200

    Add insert test

---
+++ src/main/java/com/btrco/mongodbCourse/InsertTest.java
@@ -0,0 +1,29 @@
+package com.btrco.mongodbCourse;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public class InsertTest {
+    public static void main(String[] args) {
+        MongoClient client = new MongoClient();
+        MongoDatabase database = client.getDatabase("course");
+        MongoCollection<Document> collection = database.getCollection("insertTest");
+
+        collection.drop();
+
+        Document smith = new Document("name", "Smith")
+                .append("age", 30)
+                .append("profession", "programmer");
+
+        Document jones = new Document("name", "Jones")
+                .append("age", 25)
+                .append("profession", "designer");
+
+//        collection.insertOne(smith); //if one document
+        collection.insertMany(Arrays.asList(smith, jones)); //if several documents
+    }
+}
